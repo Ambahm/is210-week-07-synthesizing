e99e891c56561d3a408156203d4c2247581f20f4
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-03-15 20:54:16 +0000

    test task1

--- task_02.py
+++ task_02.py
@@ -4,20 +4,14 @@
 
 
 def prepare_email(appointments):
-
-    """Just some doc string for function.
-
+    """ Just some docstring sample
     Arg:
-
     Return:
-
     Example:
     
     """
-    output = []
-    name = ''
-    body = 'Dear {},\nI look forward to meeting with you on {}.\nBest,\nMe.'
+    email = []
+    body = 'Dear {},\nI look forward to meeting with you on {}.\nBest,\nMe'
     for i in appointments:
-        name += body.format(i[0],i[1])
-        output = [name]
-    return output
+        email += [body.format(i[0],i[1])]
+    return email
