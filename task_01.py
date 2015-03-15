06b613d8da60a5d0b5b5783e75a2b6e49945ae75
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-03-15 03:25:36 +0000

    test

--- task_01.py
+++ task_01.py
@@ -1,14 +1,13 @@
 #!/urs/bin/env python
 # -*- coding: utf-8 -*-
-"""This is a Party Organizer, Tables of 6 Members"""
-
-families = [['Jan'], ['Jen', 'Jess'], ['Jem', 'Jack', 'Janis'],['jack','prob']]
-
+"""This is a Party Organizer, Tables of 6 Members, 1 Family per table"""
 
 def get_party_stats(families, table_size=6):
+    headcount = 0
+    table = 0
     for i in families:
-        families = len(i[:])
-        print families
+        headcount += len(i)
+        table = float(table_size)
+    return headcount, table
 
-    
-get_party_stats(families)
+print get_party_stats([['Jan'], ['Jen', 'Jess'], ['Jem', 'Jack', 'Janis']])
