1dbec46f23915fb97c30c34d8ce7b675d63e6b6e
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-03-15 17:55:33 +0000

    testing task2 w/ out docstring

--- task_02.py
+++ task_02.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 """Just a Docstring"""
 
-def prepare_email(appoitments):
-    appointments = [(),()]
-    body = 'Dear{0},\nI look forward to meeting with you on{0}.\nBest,\nMe'
+def prepare_email(appointments):
+    body = ''
     for i in appointments:
-        appointments = body.format(i[0:])
-        print appointments
-print prepare_email([('Bruno','2015'),('Reeses','March 3')])
+        body += 'Dear{0:1},\nI look forward to meeting with you on{1:1}.\nBest,\nMe.'.format(i[0],i[1])
+    return body
+
+print prepare_email([('Bruno','June 3'),('Reeses','March 3')])
