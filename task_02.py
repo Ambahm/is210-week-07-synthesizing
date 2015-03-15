16cad7eaf1f23a8dd57ae43b3934ad834519631d
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-03-15 19:58:47 +0000

    testing

--- task_02.py
+++ task_02.py
@@ -2,10 +2,23 @@
 # -*- coding: utf-8 -*-
 """Just a Docstring"""
 
-def prepare_email(appointments):
-    body = ''
-    for i in appointments:
-        body += 'Dear{0:1},\nI look forward to meeting with you on{1:1}.\nBest,\nMe.'.format(i[0],i[1])
-    return body
 
-print prepare_email([('Bruno','June 3'),('Reeses','March 3')])
+def prepare_email(appointments):
+
+    """Just some doc string for function.
+
+    Arg:
+
+    Return:
+
+    Example:
+    
+    """
+    output =[]
+    email = ''
+    body = 'Dear {},\nI look forward to meeting with you on {}.\nBest,\nMe.'
+    for i in appointments:
+        email += body.format(i[0],i[1])
+        output.append(email)
+    return output
+prepare_email([('Jen', '2015'), ('Max', 'March 3')])
